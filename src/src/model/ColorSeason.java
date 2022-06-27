b4248b1ae622681d54a39bf33c7713f56cc63a18
dojo2022-yamadashunya <103239282+dojo2022-yamadashunya@users.noreply.github.com>
2022-06-27 15:13:17 +0900

    yamada

--- src/src/model/ColorSeason.java
+++ src/src/model/ColorSeason.java
@@ -34,82 +34,83 @@ public class ColorSeason {
 	public ColorSeason() {
 		this(false, false, false, false, false, false, false, false, false, false, false, false, false);
 	}
-	public boolean isSpring() {
+	public boolean getSpring() {
 		return spring;
 	}
 	public void setSpring(boolean spring) {
 		this.spring = spring;
 	}
-	public boolean isSummer() {
+	public boolean getSummer() {
 		return summer;
 	}
 	public void setSummer(boolean summer) {
 		this.summer = summer;
 	}
-	public boolean isAutumn() {
+	public boolean getAutumn() {
 		return autumn;
 	}
 	public void setAutumn(boolean autumn) {
 		this.autumn = autumn;
 	}
-	public boolean isWinter() {
+	public boolean getWinter() {
 		return winter;
 	}
 	public void setWinter(boolean winter) {
 		this.winter = winter;
 	}
-	public boolean isWhite() {
+	public boolean getWhite() {
 		return white;
 	}
 	public void setWhite(boolean white) {
 		this.white = white;
 	}
-	public boolean isBlack() {
+	public boolean getBlack() {
 		return black;
 	}
 	public void setBlack(boolean black) {
 		this.black = black;
 	}
-	public boolean isGrey() {
+	public boolean getGrey() {
 		return grey;
 	}
 	public void setGrey(boolean grey) {
 		this.grey = grey;
 	}
-	public boolean isBeige() {
+	public boolean getBeige() {
 		return beige;
 	}
 	public void setBeige(boolean beige) {
 		this.beige = beige;
 	}
-	public boolean isRed() {
+	public boolean getRed() {
 		return red;
 	}
 	public void setRed(boolean red) {
 		this.red = red;
 	}
-	public boolean isBlue() {
+	public boolean getBlue() {
 		return blue;
 	}
 	public void setBlue(boolean blue) {
 		this.blue = blue;
 	}
-	public boolean isGreen() {
+	public boolean getGreen() {
 		return green;
 	}
 	public void setGreen(boolean green) {
 		this.green = green;
 	}
-	public boolean isYellow() {
+	public boolean getYellow() {
 		return yellow;
 	}
 	public void setYellow(boolean yellow) {
 		this.yellow = yellow;
 	}
-	public boolean isOther() {
+	public boolean getOther() {
 		return other;
 	}
 	public void setOther(boolean other) {
 		this.other = other;
 	}
+
 }
